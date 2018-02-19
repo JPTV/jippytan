2dca0731f81255b9e17a9d8f73ff0546bde6ae1e
JonnyAxehandle <jonny@axehandleunlimited.org>
2018-02-19 09:46:41 -0500

    Added cheerup

--- bot.js
+++ bot.js
@@ -10,8 +10,18 @@ client.on("ready", () => {
 client.on("message", (message) => {
 	if (message.author.bot) return;
 	var response = chain.parse(message);
-	if (response) {
+	if (!response) {
+		return;
+	}
+	
+	if (typeof response === "string") {
 		message.channel.send(response);
+	}
+	
+	if (Array.isArray(response)) {
+		for (var i in response) {
+			message.channel.send(response[i]);
+		}
 	}
 });
 
